1612307b566fc7a1596c0b5a96f518581de8020c
ArieCreson <130147448+ArieCreson@users.noreply.github.com>
2025-04-20 15:01:16 +0300

    Update my_csvwrite.m

--- DPD_Orthogonal_Basis_Implementation/my_csvwrite.m
+++ DPD_Orthogonal_Basis_Implementation/my_csvwrite.m
@@ -1,4 +1,1 @@
-clear all;
-load('C:\DPD Experiment\Experiment try\fxp_40MHz_sample_rate_H7B20L1000.mat');
-M=[real(Y) imag(Y)]; % matrix with 2 columns
-csvwrite('fxp_40MHz_sample_rate_H7B20L1000.csv',M);
+
