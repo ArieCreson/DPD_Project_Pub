6e4406d3cc260c707b48b35150225dad9f4c73b8
ArieCreson <130147448+ArieCreson@users.noreply.github.com>
2025-05-05 19:19:13 +0300

    Delete DPD_Orthogonal_Basis_Implementation directory

--- DPD_Orthogonal_Basis_Implementation/NSME_after_final_alignment.m
+++
@@ -1,8 +0,0 @@
-
-% NSME calculation
-
-options = optimset('TolX',1e-2);
-fun = @(n)10*log10(mean(abs(((Z_aligned.*n(1))+n(2))-Y_aligned).^2) / mean((abs((Z_aligned.*n(1))+n(2)).^2)));
-n0 = [1,0];
-
-[n,NSME_after_final_alignement] = fminsearch(fun,n0,options)
